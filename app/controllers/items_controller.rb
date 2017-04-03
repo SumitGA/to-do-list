8069810c4bf21a6481c4959ceccdcc4291d64a4b
SumitG <sghost33@gmail.com>
2017-04-03 17:44:08 +0545

    Add Completed_at ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,13 @@ class ItemsController < ApplicationController
 	def edit
 	end
 
+	def complete
+		@item = Item.find(params[:id])
+		@item.update_attribute(:completed_at, Time.now)
+		redirect_to root_path
+
+	end
+
 	def update
 		if @item.update(item_params)
 			redirect_to item_path(@item)
